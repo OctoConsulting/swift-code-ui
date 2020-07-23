9d9a24001c9f2c28300e2890d3d89e786e7c3982
ruchir <ruchir.mehta@gsa.gov>
2020-07-23 16:42:31 -0400

    swift-code merge from personal branch.

---
+++ src/app/form/form-service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn : 'root'})
+export class FormService{
+
+    constructor(private http: HttpClient){
+
+    }
+
+    retrieveResults(params : any): Observable<any>{
+        const options = {
+            params: params
+        }
+        const url = 'swiftcode/v1/entity/get';
+        return this.http.get(url, options);
+    }
+
+    retrieveResultsV2(params : any): Observable<any>{
+        const options = {
+            params: params
+        }
+        const url = 'swiftcode/v1/entity/getdata';
+        return this.http.get(url, options);
+    }
+
+    retrievefardfar(): Observable<any>{
+        const url = 'swiftcode/v1/fetch/fardfarlist';
+        return this.http.get(url);
+    }
+
+    downloadAll(): Observable<Blob>{
+        const url = 'swiftcode/v1/entity-csv';
+        return this.http.get(url, { responseType: 'blob' });
+    }
+
+    downloadDate(date: string): Observable<Blob>{
+        const url = 'swiftcode/v1/entity-csv/date';
+        const params = {
+            date: date
+        }
+        return this.http.get(url, {params: params, responseType: 'blob' });
+    }
+
+    downloadSearched(params: any): Observable<Blob>{
+        const url = 'swiftcode/v1/entity/get/csvdownload';
+        return this.http.get(url, {params: params, responseType: 'blob' });
+    }
+
+}
